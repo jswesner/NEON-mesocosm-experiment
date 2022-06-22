9da7c7eb555a6a508db2e03b8f17696f0366c179
Jeff Wesner <jeff.wesner@usd.edu>
2022-06-22 13:09:37 -0500

    update data

--- code/1_get_data.R
+++ code/1_get_data.R
@@ -9,34 +9,35 @@ drive_download("NEON mesocosm study 2022/temperature",
                path = "data/temperature.csv",
                overwrite = T)
 
+treatments = read_csv("data/treatments.csv") %>% 
+  mutate(treatment = paste0(temp_treat, "_",nutrient_treat))
+
 temperature <- read_csv("data/temperature.csv") %>% 
   mutate(temp_deg_c = (temp_deg_f - 32)*(5/9),
-         date_time = parse_date_time(paste(date, time), orders = "Ymd HMS"))
+         date_time = parse_date_time(paste(date, time), orders = "Ymd HMS")) %>% 
+  select(-heater) %>% 
+  left_join(treatments) %>% 
+  mutate(temp_deg_ysi = case_when(is.na(temp_from_ysi_deg_c) ~ temp_deg_c, TRUE ~ temp_from_ysi_deg_c)) 
 
-heaters = temperature %>% filter(date == "2022-06-15") %>% 
-  distinct(tank, heater)
+
+
 
 
 # Save o2 measures --------------------------------------------------------
 
 temperature %>%
-  select(-heater) %>% 
-  left_join(heaters) %>% 
   clean_names() %>% 
   filter(!is.na(o2_do_mg_l)) %>% 
-  mutate(temp_deg_ysi = case_when(is.na(temp_from_ysi_deg_c) ~ temp_deg_c, TRUE ~ temp_from_ysi_deg_c)) %>% 
   select(tank, date_time, o2_do_mg_l, temp_deg_c) %>% 
   write_csv(file = "data/o2_temp.csv")
 
 
 # Plot temperature --------------------------------------------------------
 temperature %>% 
-  select(-heater) %>% 
-  left_join(heaters) %>%
   filter(date >= "2022-05-27") %>%
   drop_na(temp_deg_f) %>% 
   drop_na(temp_deg_c) %>% 
-  ggplot(aes(x = date_time, y = temp_deg_c, color = heater)) + 
+  ggplot(aes(x = date_time, y = temp_deg_c, color = temp_treat)) + 
   geom_point() + 
   geom_line(aes(group = tank), alpha = 0.5) +
   scale_color_colorblind() + 
@@ -45,13 +46,11 @@ temperature %>%
   NULL
 
 temperature %>%
-  filter(date >= "2022-05-27") %>%
-  select(-heater) %>% 
-  left_join(heaters) %>% 
-  group_by(date_time, heater) %>% 
+  filter(date >= "2022-05-27") %>% 
+  group_by(date_time, temp_treat) %>% 
   summarize(mean = mean(temp_deg_c, na.rm = T)) %>% 
-  pivot_wider(names_from = heater, values_from = mean) %>% 
-  mutate(diff = y - n) %>%
+  pivot_wider(names_from = temp_treat, values_from = mean) %>% 
+  mutate(diff = heated - ambient) %>%
   drop_na(diff) %>% 
   ggplot(aes(x = date_time, y = diff)) +
   geom_line()+
@@ -64,9 +63,7 @@ temperature %>%
 temperature %>%
   glimpse() %>% 
   filter(date >= "2022-05-27") %>%
-  select(-heater) %>% 
-  left_join(heaters) %>% 
-  ggplot(aes(x = temp_from_ysi_deg_c, y = temp_deg_c, color = heater)) + 
+  ggplot(aes(x = temp_from_ysi_deg_c, y = temp_deg_c, color = temp_treat)) + 
   geom_point() +
   ylim(0, 40) +
   xlim(0, 40) + 
@@ -77,11 +74,10 @@ temperature %>%
 # Plot O2_temp ------------------------------------------------------------
 
 temperature %>%
-  select(-heater) %>% 
-  left_join(heaters) %>% 
   clean_names() %>% 
   filter(!is.na(o2_do_mg_l)) %>% 
   ggplot(aes(y = o2_do_mg_l, x = date_time)) +
-  geom_point() + 
-  geom_line(aes(group = tank))
+  geom_point(aes(color = nutrient_treat)) + 
+  geom_line(aes(group = tank, color = nutrient_treat)) + 
+  facet_grid(temp_treat ~ row)
 
